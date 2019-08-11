ebab943588dcc0f086a152da37ea8bed5d9fc89a
Vishaal <vishaal@handimaps.com>
2019-08-10 20:24:01 -0700

    changed static json path

---
+++ client/build/precache-manifest.58aba5873b88208c1dfafb349a5c4e06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7a386bd4371bcbf362adf3fc0c844a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3064f0c00528041d9b2c",
+    "url": "/static/css/main.e12d6608.chunk.css"
+  },
+  {
+    "revision": "cb4ea65e839028fb2cf5",
+    "url": "/static/js/2.c74bc913.chunk.js"
+  },
+  {
+    "revision": "3064f0c00528041d9b2c",
+    "url": "/static/js/main.2921e5bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
